e8da313d2c816540d1ae7543bfdcf6e7cf2f8afc
Ziyuan Zhao <ziy02@student.ubc.ca>
2025-03-01 14:14:51 -0800

    import os

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 from src.layout import *
 from src.callbacks import *
+import os
 
 # Initialize Dash application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -16,6 +17,6 @@ register_callbacks(app)
 
 # Run application
 if __name__ == "__main__":
-    # app.run_server(debug=True)
+    #app.run_server(debug=True)
     port = int(os.environ.get("PORT", 8080))  # Use port provided by Render
     app.run_server(debug=False, host="0.0.0.0", port=port)
