484c45b53e6676a7de9142c7c4cb6d60bd9fc7a3
Jenny Zhang <yucizhang2008@gmail.com>
2025-03-09 01:13:56 -0800

    contrast nasdaq100 link color with sidebar background

--- src/layout.py
+++ src/layout.py
@@ -58,8 +58,8 @@ sidebar = [
     html.A(
         "NASDAQ 100 Index ETF", 
         href="https://www.invesco.com/us/financial-products/etfs/product-detail?audienceType=Investor&productId=ETF-QQQM", 
-        className="text-primary", 
-        style={"color": "red"}
+        # className="text-primary", 
+        style={"color": "yellow"}
     ),
     
     html.Hr(),  # Optional line separator
