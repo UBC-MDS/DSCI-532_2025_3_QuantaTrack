4155eec01b34935df0e252c15a69f0209e2b5569
Jenny Zhang <yucizhang2008@gmail.com>
2025-03-12 20:08:03 -0700

    address issue #100 add comment at bottom about interactivity to avoid function confusion

--- src/layout.py
+++ src/layout.py
@@ -238,7 +238,7 @@ table = AgGrid(
     columnDefs=ag_columns,
     rowData=[],  # 初始数据为空，后续由回调更新
     defaultColDef={'filter': True},  # 默认启用列过滤
-    style={"height": 600},
+    style={"height": 590},
     dashGridOptions={"pagination": True, "paginationAutoPageSize": True},
 )
 
@@ -273,7 +273,11 @@ tabs = dbc.Tabs([
                 dbc.Col(scatter_plot)
             ], class_name="g-0"),
         ],
-        label="Overview"
+        label="Overview",
+        # style={
+        #     'height': '100%',  # Ensures the height fills the container
+        #     'overflow': 'hidden',  # Prevent scrolling
+        # }
     ),
 
     dbc.Tab(
@@ -305,15 +309,39 @@ tabs = dbc.Tabs([
                 dbc.Col(price_trend_graph)
             ], class_name="g-0", style={"marginTop": "20px"}),
         ],
-        label="Stock"
+        label="Stock",
+        # style={
+        #     'height': '100%',  # Ensures the height fills the container
+        #     'overflow': 'hidden',  # Prevent scrolling
+        # }
     ),
     
     dbc.Tab(
         [
             search_download_row,
             dbc.Row(dbc.Col(table)),
+            # Add a row at the bottom with the updated interactivity message
+            dbc.Row(
+                dbc.Col(
+                    html.Div(
+                        "Disclaimer: Refresh Time Interactivity is only available during trading hours, from 9:30 AM - 4:00 PM (Eastern Daylight Time, GMT-04:00), Monday to Friday.",
+                        style={
+                            "textAlign": "center",
+                            "fontStyle": "italic",
+                            "marginTop": "10px",
+                            "fontSize": "14px",
+                            "color": "#888",  # Gray text color
+                        }
+                    ),
+                    width=12,
+                ),
+            ),
         ],
-        label="Data"
+        label="Data",
+        style={
+            'height': '100%',  # Ensures the height fills the container
+            'overflow': 'hidden',  # Prevent scrolling
+        }
     )
 ])
 
